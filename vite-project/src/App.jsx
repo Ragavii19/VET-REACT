aea40808c3142d09eef4d7df506a2d55eb54bdf0
Ragavii19 <ragavij25csa@gamail.com>
2026-06-18 10:30:59 +0530

    FORM

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,6 +8,7 @@ function App(){
   
   const[students,setStudents] = useState("[]");
   const[editIndex,setEditIndex] = useState("null");
+
   useEffect(() => {
 
     const savedStudents = 
@@ -16,10 +17,91 @@ function App(){
     ) || [];
     setStudents(savedStudents);
  },[]);
+
   const handleSubmit = () => {
-     if 
-  }
-}
+    if (
+      name === "" ||
+      email === "" ||
+      department === "" ||
+      age === ""        
+    )
+    {
+      alert("Please fill all fields");
+      return;
+    }
+      const student = {
+        name,
+        email,
+        department,
+        age
+      };
+      if(editIndex !== null){
+        const updatedStudents = [...students];
+
+        updatedStudents[editIndex] = students;
+
+        setStudents(updatedStudents);
+
+        localStorage.setItem(
+          "students",
+          JSON.stringify(updatedStudents)
+        );
+        setEditIndex(null);
+      }
+      else {
+        const updatedStudents = [
+          ...students,
+          student
+        ];
+        setStudents(updatedStudents);
+        localStorage.setItem(...);
+      }
+      localStorage.setItem(
+        "students",
+        JSON.stringify(updatedStudents)
+      );
+    }
+      setName("");
+      setEmail("");
+      setDepartment("");
+      setAge("");
+    };
+      const handleEdit = (index) => {
+        const student = student[index];
+        setName(student.name);
+        setEmail(student.email);
+        setDepartment(student.department);
+        setAge(student.age);
+        setEditIndex(index);
+      };
+      const handleDelete =(index) => {
+        const updatedStudents =
+        students.filter(
+          (student,i) => i !== index
+        );
+        setStudents(updatedStudents);
+        localStorage.setItem(
+          "students",
+          JSON.stringify(updatedStudents)
+        );
+      };
+      const inputStyle ={
+        width : "100%",
+        padding : "10px",
+        marginBottom : "15px",
+        borderRadius : "5px",
+        border : "1px solid #ccc",
+        boxSizing : "border-box"
+      };
+      const buttonStyle = {
+        width : "100%",
+        padding : "12px",
+        border : "none",
+        borderRadius : "5px",
+        cursor : "pointer"
+      };
+      export default App;
+
   return( 
     <div 
     style={{
@@ -159,4 +241,3 @@ function App(){
     </table>
     </div>
   );
-}
